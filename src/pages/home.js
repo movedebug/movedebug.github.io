3ee42189c6700bc8baaefcea738aa8baa7e80628
v1xingyue <qixingyue@126.com>
2022-11-01 11:05:18 +0800

    cra basic

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,26 @@
 import StarMaskOnboarding from '@starcoin/starmask-onboarding';
 import { Button, Col, Row, Statistic } from 'antd';
 import { useEffect, useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { setInitial, setAccount } from "../feature/global";
+import { providers } from "@starcoin/starcoin";
+
+export const COUNTER_ADDRESS = "0x1168e88ffc5cec53b398b42d61885bbb"
+export const COUNTER_RESOURCE_ID = "MyCounter::Counter"
+export const INCR_COUNTER_FUNC_NAMW = "MyCounter::incr_counter"
+export const INCR_COUNTERBY_FUNC_NAME = "MyCounter::incr_counter_by"
+
+const getResource = async (address, functionId) => {
+
+    let starcoinProvider = new providers.Web3Provider(
+        window.starcoin,
+        "any"
+    );
+
+    const resourceType = `${address}::${functionId}`
+    const resource = await starcoinProvider.getResource(address, resourceType)
+    console.log(resource)
+    return resource
+}
+
 
 const initialStarCoin = () => {
     const currentUrl = new URL(window.location.href);
@@ -14,7 +32,8 @@ const initialStarCoin = () => {
 
     let onboarding;
     try {
-        onboarding = new StarMaskOnboarding({ forwarderOrigin });
+        const
+            onboarding = new StarMaskOnboarding({ forwarderOrigin });
     } catch (error) {
         console.error(error);
     }
@@ -55,6 +74,9 @@ const Home = () => {
         });
     };
 
+
+    const [counter, updateCounter] = useState(100);
+
     return (
         <Row>
             <Col span={6}>
@@ -72,6 +94,18 @@ const Home = () => {
                         :
                         <Button type='primary' onClick={connectStarmask}>Connect Starmask</Button>
                 }
+            </Col>
+
+            <Col span={6}>
+                <p>Current Value : <b>{counter}</b> </p>
+                <p>
+                    <Button type='primary' onClick={() => {
+                        updateCounter(-1);
+                    }}>Load From Contract</Button>
+                    <Button onClick={() => {
+                        updateCounter(counter + 5);
+                    }}>Increment</Button>
+                </p>
             </Col>
         </Row >
     );
